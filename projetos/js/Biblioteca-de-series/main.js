2910bb65ff3ff6f8af2978075424d6b61e194524
VaiNaWeb <johndoe@example.com>
2019-07-30 17:52:30 -0300

    Inserindo input da temporada

--- projetos/js/Biblioteca-de-series/main.js
+++ projetos/js/Biblioteca-de-series/main.js
@@ -1,15 +1,17 @@
-let cards = []
+let cards = [];
+let temps = [];
 let isEditing = undefined;
 let form = document.createElement("form");
 let ul = document.createElement("ul");
+let tempList = document.createElement("ul");
 let sectionCard = document.createElement("section");
 let inputSerie = document.createElement("input"); 
 let inputDescription = document.createElement("input"); 
+let inputEpisode =document.createElement("input");
 let buttonEpisode = document.createElement("button"); 
 let buttonSubmit = document.createElement("button");
 
 
- 
 function renderItem (){
  
   let label = document.createElement("label");  
@@ -19,19 +21,52 @@ function renderItem (){
   inputSerie.setAttribute("required", "true");
   inputDescription.setAttribute("placeholder", "Sinopse da Série");
   inputDescription.setAttribute("required", "true");
+  buttonEpisode.setAttribute('onclick', "showAddTemp()")
 
-  buttonEpisode.innerHTML = "Novo Episódio";
+  buttonEpisode.innerHTML = "Nova Temporada";
   buttonSubmit.innerHTML = "Adicionar Série";  
   title.innerHTML = "Bliblioteca de Séries";
 
   form.appendChild(label);
   form.appendChild(title);
-
   form.appendChild(inputSerie);
   form.appendChild(inputDescription);
-  form.appendChild(buttonEpisode);
-  form.appendChild(buttonSubmit); 
+  form.appendChild(buttonEpisode);   
+  form.appendChild(buttonSubmit);
   document.body.appendChild(form);
+
+  renderTemps = () => {
+    event.preventDefault();
+  
+    if (inputEpisode.value !== '') {
+      let tempBox = [inputEpisode.value];
+  
+      temps.push({
+        temp: inputEpisode.value,
+      });
+  
+      tempBox.map((item) => {
+        
+        let tempItem = document.createElement('div');
+        let title2 = createTag('span', 'p');
+        let deleteTempButton = createTag('button', 'x');
+  
+        title2.innerHTML = item;
+        title2.style.color = "white";
+        deleteTempButton.innerHTML = "X"
+  
+        deleteTempButton.setAttribute('onclick', 'deleteTempAdded()');     
+  
+        tempList.appendChild(tempItem);
+        tempItem.appendChild(title2);      
+        tempItem.appendChild(deleteTempButton);
+        
+        form.appendChild(tempList);
+        
+        inputEpisode.value = "";
+      });
+    }  
+  }
 
   buttonSubmit.addEventListener("click", function(){	
     event.preventDefault();
@@ -41,26 +76,32 @@ function renderItem (){
     }
     else{
       cards.push({
+        temps,
         description: inputDescription.value,
         title: inputSerie.value,
+      
       })
     }    
 
     inputSerie.value = "";
     inputDescription.value = "";
+    temps = [];
     render();
   });
+
 }
 
 function editCard (){
   cards[isEditing] = {
     description: inputDescription.value,
-    title: inputSerie.value
+    title: inputSerie.value,
+    temps,
   };
 
   isEditing = undefined;
   inputSerie.value = "";
   inputDescription.value = "";
+  temps = [];
   render()
 }
 
@@ -68,6 +109,34 @@ function deletCard(cards, item){
   return cards.filter(function(el){
     return el != item;
   });
+}
+
+function deleteTempAdded() {
+	event.preventDefault();
+	let tempAdded = event.target.parentNode;
+	tempAdded.remove();
+}
+showAddTemp = () => {
+  event.preventDefault();
+  let addTemp = createTag('button', 'p');
+  addTemp.innerHTML = "Add Temporada";
+
+	addTemp.addEventListener('click', function() {
+		event.preventDefault();
+		renderTemps();
+  });
+  
+	buttonEpisode.style.display = 'none';
+	form.appendChild(addTemp);
+	form.appendChild(inputEpisode);
+}
+function createTag (el,txt){
+  let listItem = document.createElement(el);
+  let itemText = document.createElement(txt);
+
+  listItem.appendChild(itemText)
+
+  return listItem;
 }
 
 function render(){
@@ -78,6 +147,7 @@ function render(){
   cards.map((item, index) => {
     
     let li = document.createElement("li");
+    let serieItem = document.createElement('p');
     let elementSerieTitle = document.createElement("p");
     elementSerieTitle.innerHTML = item.title;
     // let elementSerieDescription = document.createTextNode(item.title);
@@ -89,12 +159,21 @@ function render(){
 
     deletList.innerHTML = "Excluir";
     editList.innerHTML = "Editar";
-    li.classList.add("titulo")
 
-    // elementSerieTitle.appendChild(elementSerieDescription)
-    // elementInfoTitle.appendChild(elementInfoDescription)   
     li.appendChild(elementSerieTitle)
     li.appendChild(elementInfoTitle)
+
+    item.temps.map((temp => {
+      let temporada = createTag('p', 'span');
+      temporada.innerHTML = temp.temp
+
+      serieItem.appendChild(temporada);      
+      li.appendChild(serieItem)
+		}));
+
+		tempList.innerHTML = '';
+    temps = [];
+
     li.appendChild(deletList)
     li.appendChild(editList)
     ul.appendChild(li)
@@ -107,7 +186,6 @@ function render(){
     editList.addEventListener('click', function(){
       inputDescription.value = item.description
       inputSerie.value = item.title
-
       isEditing = index;
     })
     
